b08ca55ca465fe8c082d37523947d823130a8fcb
Simranjeet Singh <hey@simranjeetsingh.dev>
2020-10-07 11:52:11 +0530

    [Adding an index file and a driver to work with]

---
+++ src/Driver.js
@@ -0,0 +1,68 @@
+/**
+ * Database Driver
+ * Wraps the native pg driver and only exposes the friendly functions
+ * do not use it directly. exported for testing purposes only
+ */
+export class DbDriver {
+
+  constructor(pool, logger) {
+    this._pool = pool;
+    this._logger = logger;
+  }
+
+  /**
+   * wrapper function to execute queries.
+   * @param qry 
+   * @param values 
+   */
+  async query(qry, values = []) {
+    try {
+      const params = {
+        "text": qry,
+        "values": values
+      };
+      const {rows} = await this._pool.query(params);
+
+      return rows;
+    } catch(err) {
+      this._logger.error({
+        "message": "There was an error in db operation",
+        "error": err
+      });
+      throw err;
+    }
+  }
+
+  /**
+   * Returns a wrappedClient for running transactions on database.
+   * Once a transaction is started, any error will bring the transaction in the hang state
+   * Make sure you are handling commit and rollback in proper way
+   */
+  async startTransaction() {
+    const client = await this._pool.connect();
+
+    await client.query("begin");
+
+    const wrappedClient = {
+      "query": async (qry, values = []) => {
+        const params = {
+          "text": qry,
+          "values": values
+        };
+        const {rows} = await client.query(params);
+  
+        return rows;
+      },
+      "commit": async () => {
+        await client.query("commit");
+        client.release();
+      },
+      "rollback": async () => {
+        await client.query("rollback");
+        client.release();
+      }
+    };
+
+    return wrappedClient;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { Pool } = require("pg");
+const getLogger = require("pino");
+const {Pool} = require("pg");
+
+/**
+ * Extracts the config required to create a database connection
+ * @param {*} config 
+ */
+function getDbConfig(config) {
+  // validate config and throw error
+  return {
+    "database": config.database,
+    "host": config.host,
+    "password": config.password,
+    "port": config.port,
+    "user": config.user
+  };
+}
+
+/**
+ * function to generate an instance of db driver
+ * Used by db middleware
+ * @param pool 
+ * @param logger 
+ */
+function getDbDriver(config) {
+  const {logger} = config;
+  const queryLogger = logger || getLogger();
+  const dbConfig = getDbConfig(config);
+  const pool = new Pool(dbConfig);
+  return new DbDriver(pool, queryLogger);
+}
+
+module.exports = {
+  getDbDriver
+};
