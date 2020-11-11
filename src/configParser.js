8d9f19fb6776b3f420d1a200a441a697cf5a56e9
Simranjeet Singh <hey@simranjeetsingh.dev>
2020-11-11 10:08:26 +0530

    [Adding a config parser for validation]

---
+++ src/configParser.js
@@ -0,0 +1,48 @@
+const Joi = require("joi");
+
+const configSchema = Joi.object().keys({
+  "host": Joi.string().required(),
+  "database": Joi.string().required(),
+  "port": Joi.number().integer().greater(0).required(),
+  "password": Joi.string().required(),
+  "user": Joi.string().required(),
+  "statementTimeout": Joi.number().integer(),
+  "queryTimeout": Joi.number().integer(),
+  "connectionTimeoutMillis": Joi.number().integer(),
+  "idleInTransactionSessionTimeout": Joi.number().integer()
+});
+
+/**
+ * Extracts the config required to create a database connection
+ * @param {*} config 
+ */
+function getDbConfigs(config) {
+  const rwConfig = {
+    "database": config.database,
+    "host": config.host,
+    "password": config.password,
+    "port": config.port,
+    "user": config.user,
+    "ssl": config.ssl,
+    "statement_timeout": config.statementTimeout || 5000,
+    "query_timeout": config.queryTimeout || 10000,
+    "connectionTimeoutMillis": config.connectionTimeoutMillis || 5000,
+    "idle_in_transaction_session_timeout": config.idleInTransactionSessionTimeout || 12000
+  };
+
+  const roConfig = config.roHost? Object.assign({}, rwConfig, {"host": config.roHost}) : null;
+
+  return {rwConfig, roConfig};
+}
+
+function validateConfig(config) {
+  const res = configSchema.validate(config);
+  if(res.error) {
+    throw res.error;
+  }
+}
+
+module.exports = {
+  getDbConfigs,
+  validateConfig
+};
--- src/index.js
+++ src/index.js
@@ -2,30 +2,7 @@ const getLogger = require("pino");
 const {Pool} = require("pg");
 
 const DbDriver = require("./Driver");
-
-/**
- * Extracts the config required to create a database connection
- * @param {*} config 
- */
-function getDbConfigs(config) {
-  // validate config and throw error
-  const rwConfig = {
-    "database": config.database,
-    "host": config.host,
-    "password": config.password,
-    "port": config.port,
-    "user": config.user,
-    "ssl": config.ssl,
-    "statement_timeout": config.statementTimeout || 5000,
-    "query_timeout": config.queryTimeout || 10000,
-    "connectionTimeoutMillis": config.connectionTimeoutMillis || 5000,
-    "idle_in_transaction_session_timeout": config.idleInTransactionSessionTimeout || 12000
-  };
-
-  const roConfig = config.roHost? Object.assign({}, rwConfig, {"host": config.roHost}) : null;
-
-  return {rwConfig, roConfig};
-}
+const {getDbConfigs, validateConfig} = require("./configParser");
 
 function getConnectedPool(config) {
   const {rwConfig, roConfig} = getDbConfigs(config);
@@ -42,6 +19,7 @@ function getConnectedPool(config) {
  * @param logger 
  */
 function getDbDriver(config) {
+  validateConfig(config);
   const {logger} = config;
   const queryLogger = logger || getLogger();
   const {rwPool, roPool} = getConnectedPool(config);
