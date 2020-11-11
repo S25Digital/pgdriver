e38e98fd4522626bbc77e10cc88ca6b99675cf9c
Simranjeet Singh <hey@simranjeetsingh.dev>
2020-11-11 11:16:14 +0530

    [Adding a connection map to retunr the same pool]

--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ const {Pool} = require("pg");
 const DbDriver = require("./Driver");
 const {getDbConfigs, validateConfig} = require("./configParser");
 
+const cachedConnectionMap = new Map();
+
 function getConnectedPool(config) {
   const {rwConfig, roConfig} = getDbConfigs(config);
-  const rwPool = new Pool(rwConfig);
-  const roPool = roConfig === null ? rwPool : new Pool(roConfig);
+
+  let rwPool = cachedConnectionMap.get(`${rwConfig.host}-${rwConfig.database}`) || null;
+  let roPool = roConfig === null ? rwPool : (cachedConnectionMap.get(`${roConfig.host}-${roConfig.database}`) || null);
+
+  // return cached instance if any
+  if(rwPool !== null && roPool !== null) {
+    return {rwPool, roPool};
+  }
+
+  // No cached instance of pool, create a pool and cache it
+  rwPool = new Pool(rwConfig);
+  roPool = roConfig === null ? rwPool : new Pool(roConfig);
+
+  cachedConnectionMap.set(`${rwConfig.host}-${rwConfig.database}`, rwPool);
+
+  if(roConfig !== null) {
+    cachedConnectionMap.set(`${roConfig.host}-${roConfig.database}`, roPool);
+  }
 
   return {rwPool, roPool};
 }
